541b9d0079ff529e4b0870b6a5168a5524d31617
abeer khakwani & kelly de vries <student@epicodus.com>
2015-03-04 10:57:58 -0800

    Create Task class, add form to app, create .

--- src/task.php
+++ src/task.php
@@ -1,36 +1,26 @@
-<?php 
+<?php
 class Task
 {
     private $description;
 
-    function __construct($description)
-    {
-        $this->description = $description;    
+    function __construct($description){
+        $this->description = $description;
     }
 
-    function setDescription($new_description)
-    {
-        $this->description = (string) $new_description;
+    function setDescription($new_description){
+        $this->description =$new_description;
     }
 
-    function getDescription()
-    {
+    function getDescription(){
         return $this->description;
     }
 
-      function save()
-    {
-        array_push($_SESSION['list_of_tasks'], $this);
+    function save(){
+         array_push($_SESSION["list_description"],$this);
     }
 
-    static function getAll()
-    {
-        return $_SESSION['list_of_tasks'];
+    static function getAll(){
+        return $_SESSION["list_description"];
     }
-    static function deleteAll()
-    {
-        $_SESSION['list_of_tasks'] = array();
-    }
-
 }
 ?>
